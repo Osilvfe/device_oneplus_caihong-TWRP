aedf366f20e2d6d3d9ff05568c5463f2762189ed
Osilvfe <3072756864@qq.com>
2025-04-20 22:19:30 +0800

    first commit

---
+++ twrp_caihong.mk
@@ -0,0 +1,27 @@
+
+DEVICE_PATH := device/oneplus/caihong
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit from this product for devices that support only 64-bit apps using:
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+
+# Enable virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Enable Project Quotas and Case Folding
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Inherit from munch device
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := caihong
+PRODUCT_NAME := twrp_caihong
+PRODUCT_BRAND := OnePlus
+PRODUCT_MODEL := OPD2404
+PRODUCT_MANUFACTURER := OnePlus
